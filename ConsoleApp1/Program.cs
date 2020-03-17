9dcbbc93a6f08d7220d4aca5d9d0eb6fb13fca96
Fernando Lopes <fernandolopes.s@gmail.com>
2020-03-17 11:32:39 -0300

    primeiro commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            var connection = "Server=localhost;DataBase=testedb;Uid=root;Pwd=example";
+            var optionsBuilder = new DbContextOptionsBuilder<TestedbContext>();
+            optionsBuilder.UseMySQL(connection);
+            
+            var ctx = new TestedbContext(optionsBuilder.Options);
+
+            if(args.Length > 0)
+            {
+                Exemplo exemplo = new Exemplo { Nome = args[0], Idade = 0 };
+                ctx.Exemplos.Add(exemplo);
+                ctx.SaveChanges();
+            }
+
+            var list = from a in ctx.Exemplos let nomes = a.Nome select nomes;
+
+            foreach(var i in list)
+                Console.WriteLine("nome: {0}", i);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp1/TestedbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class TestedbContext : DbContext
+    {
+        public TestedbContext(DbContextOptions<TestedbContext> options) : base(options)
+        {
+        }
+        public DbSet<Exemplo> Exemplos { get; set; }
+    }
+}
